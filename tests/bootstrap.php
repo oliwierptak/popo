a04e97fa92dd67592247158b750550841cdb21dd
Oliwier Ptak <oliwierptak@gmail.com>
2018-08-03 20:23:31 +0200

    moved bootstrap.php to tests directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,5 @@ declare(strict_types = 1);
 
 namespace Popo;
 
-define('Popo\APPLICATION_DIR', __DIR__ . \DIRECTORY_SEPARATOR);
+define('Popo\APPLICATION_DIR', __DIR__ . '/../');
 define('Popo\TESTS_DIR', APPLICATION_DIR . 'tests/');
